6bc8ffd48f6bc7cdb5edeac087c1f6f5511ee1b6
Michael Graute <m.graute@team-braeutigam.de>
2020-04-02 00:07:25 +0200

    - added some styling to the auto generated views and examples

--- examples/detail.php
+++ examples/detail.php
@@ -1,6 +1,35 @@
 <?php
-ini_set('display_errors', true);
-error_reporting(1);
+
+use Pressmind\ORM\Object\MediaObject;
+
 require_once dirname(__DIR__) . '/bootstrap.php';
-$mediaObject = new \Pressmind\ORM\Object\MediaObject(intval($_GET['id']));
-echo $mediaObject->render('example');
+$mediaObject = new MediaObject(intval($_GET['id']));
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <style>
+        pre {
+            max-height: 400px;
+            overflow: auto;
+        }
+    </style>
+    <title>An example detail page</title>
+</head>
+<body>
+<div class="container">
+    <?php
+    //To Render a Media Object, we just need to call the Render function and give the suffix of the render script as parameter
+    //In this case the view script with the naming convention <MediaObjectType>_Example.php  (e.g. Reise_Example.php) will be called and rendered.
+    //The example view scripts for all media object types can be found in /examples/views after the install.php script has been executed.
+    echo $mediaObject->render('example');
+    ?>
+</div>
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</body>
+</html>
--- examples/index.php
+++ examples/index.php
@@ -4,6 +4,7 @@ use Pressmind\Search;
 
 require_once dirname(__DIR__) . '/bootstrap.php';
 
+//Here we list all MediaObjects within a price-range of 1 to 5000, limited to 100 results and sorted by rand()
 $tour_search = new Search(
     [
         Search\Condition\PriceRange::create(1, 5000),
@@ -17,6 +18,32 @@ $tour_search = new Search(
     ]
 );
 $tours = $tour_search->getResults();
-foreach ($tours as $tour) {
-    echo '<a href="detail.php?id=' . $tour->id . '">' . $tour->name . '</a><br>';
-}
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <style>
+        pre {
+            max-height: 400px;
+            overflow: auto;
+        }
+    </style>
+    <title>Example for displaying a list of tours</title>
+</head>
+<body>
+<div class="container">
+    <?php foreach ($tours as $tour) {
+        echo '<a href="detail.php?id=' . $tour->id . '">' . $tour->name . '</a><br>';
+    }
+    ?></div>
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</body>
+</html>
