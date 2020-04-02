6db7ee08bab1f158e75849254b97ec9fd3929aa4
Michael Graute <m.graute@team-braeutigam.de>
2020-04-02 15:47:26 +0200

    - added help texts, code cleanup

--- cli/import.php
+++ cli/import.php
@@ -9,30 +9,54 @@ $args = $argv;
 
 $importer = new Import();
 
-if($args[1] == 'fullimport') {
-    try {
-        $importer->import();
-        $importer->postImport();
-    } catch(Exception $e) {
-
-    }
+switch ($args[1]) {
+    case 'fullimport':
+        Writer::write('Importing all media objects', Writer::OUTPUT_BOTH, 'import.log');
+        try {
+            $importer->import();
+            $importer->postImport();
+            Writer::write('Import done.', Writer::OUTPUT_BOTH, 'import.log');
+        } catch(Exception $e) {
+            Writer::write($e->getMessage(), Writer::OUTPUT_BOTH, 'import_error.log');
+        }
+        break;
+    case 'mediaobject':
+        if(is_numeric($args[2])) {
+            Writer::write('Importing mediaobject ID: ' . $args[2], Writer::OUTPUT_BOTH, 'import.log');
+            try {
+                $importer->importMediaObject(intval($args[2]));
+                $importer->postImport();
+                Writer::write('Import done.', Writer::OUTPUT_BOTH, 'import.log');
+            } catch(Exception $e) {
+                Writer::write($e->getMessage(), Writer::OUTPUT_BOTH, 'import_error.log');
+            }
+        } else {
+            echo "Missing object ID or object ID is not numeric\n";
+        }
+        break;
+    case 'objecttypes':
+        if(!empty($args[2])) {
+            Writer::write('Importing objecttypes ID(s): ' . $args[2], Writer::OUTPUT_BOTH, 'import.log');
+            $ids = array_map('trim', explode(',', $args[2]));
+            try {
+                $importer->importMediaObjectTypes($ids);
+                Writer::write('Import done.', Writer::OUTPUT_BOTH, 'import.log');
+            } catch(Exception $e) {
+                Writer::write($e->getMessage(), Writer::OUTPUT_BOTH, 'import_error.log');
+            }
+        } else {
+            echo "Missing objecttype id(s)";
+        }
+        break;
+    case 'help':
+    case '--help':
+    case '-h':
+    default:
+        $helptext = "usage: import.php [fullimport | mediaobject | objecttypes] [<single id or commaseparated list of ids>]\n";
+        $helptext .= "Example usages:\n";
+        $helptext .= "php import.php fullimport\n";
+        $helptext .= "php import.php mediaobject 123456 <only single id is allowed>\n";
+        $helptext .= "php import.php objecttypes 123, 456 <singe or multiple ids allowed>\n";
+        echo $helptext;
 }
 
-if($args[1] == 'mediaobject' && is_numeric($args[2])) {
-    try {
-        Writer::write('Importing mediaobject ID: ' . $args[2], Writer::OUTPUT_BOTH, 'import.log');
-        $importer->importMediaObject(intval($args[2]));
-        $importer->postImport();
-    } catch(Exception $e) {
-        echo $e->getMessage();
-    }
-}
-
-if($args[1] == 'objecttypes' && !empty($args[2])) {
-    $ids = array_map('trim', explode(',', $args[2]));
-    try {
-        $importer->importMediaObjectTypes($ids);
-    } catch(Exception $e) {
-
-    }
-}
