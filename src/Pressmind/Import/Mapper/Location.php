38f22efc2e1e3d51f58abf90b0bac748e9fb964f
Michael Graute <m.graute@team-braeutigam.de>
2020-05-12 16:23:43 +0200

    - fixed warning because of possibly unset properties lat and lng

--- src/Pressmind/Import/Mapper/Location.php
+++ src/Pressmind/Import/Mapper/Location.php
@@ -16,8 +16,8 @@ class Location implements MapperInterface
                 $mapped_object->section_name = '';
                 $mapped_object->language = $pLanguage;
                 $mapped_object->var_name = $pVarName;
-                $mapped_object->lat = $object->lat;
-                $mapped_object->lng = $object->lng;
+                $mapped_object->lat = isset($object->lat) ? $object->lat : null;
+                $mapped_object->lng = isset($object->lat) ? $object->lng : null;
                 $mapped_object->address = $object->address;
                 $mapped_object->title = $object->title;
                 $result[] = $mapped_object;
