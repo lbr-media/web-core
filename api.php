a3ead3ce16256ad608f5aefd8fd7d083e8a92b86
Michael Graute <m.graute@team-braeutigam.de>
2020-06-15 18:02:56 +0200

    - added api endpoint, controller and class for external IBE access

---
+++ api.php
@@ -0,0 +1,27 @@
+<?php
+namespace Pressmind;
+
+use Pressmind\MVC\Request;
+use Pressmind\MVC\Response;
+use Pressmind\REST\Controller\Ibe;
+
+
+error_reporting(1);
+ini_set('display_errors', 1);
+require_once __DIR__ . DIRECTORY_SEPARATOR . 'bootstrap.php';
+
+$request = new Request();
+
+if($request->isPost()) {
+    $response = new Response();
+    $response->setContentType('application/json');
+    try {
+        $action = $request->getParameter('action');
+        $class = new Ibe($request->getParameter('data'));
+        $response->setBody(json_encode(['success' => true, 'data' => $class->$action()]));
+    } catch (\Exception $e) {
+        $response->setCode(500);
+        $response->setBody(json_encode(['success' => false, 'msg' => $e->getMessage()]));
+    }
+    $response->send();
+}
