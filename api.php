1a1ed51a5250be2b9f859cd006e7dc739be261a8
Michael Graute <m.graute@team-braeutigam.de>
2020-06-22 11:36:21 +0200

    - added support for pressmind channel update/preview

--- api.php
+++ api.php
@@ -22,3 +22,28 @@ if($request->isPost()) {
     }
     $response->send();
 }
+
+if($request->isGet()) {
+    if($request->getParameter('type') == 'import') {
+        $response = new Response();
+        $response->setContentType('application/json');
+        try {
+            $importer = new Import();
+            $importer->importMediaObject($request->getParameter('id_media_object'));
+            $media_object = new ORM\Object\MediaObject($request->getParameter('id_media_object'));
+            if($request->getParameter('preview') == "1") {
+                $config = Registry::getInstance()->get('config');
+                $preview_url = WEBSERVER_HTTP . $config['data']['preview_url'] . '?id_media_object=' . $media_object->getId() . '&preview=1';
+                $response->setContentType('text/html');
+                $response->setBody('You will be redirected to Preview Page: ' . $preview_url);
+                $response->addHeader('Location', $preview_url);
+            } else {
+                $response->setBody(json_encode(['status' => 'Code 200: Import erfolgreich', 'url' => null, 'msg' => implode("\n", $importer->getLog())]));
+            }
+        } catch (\Exception $e) {
+            $response->setCode(500);
+            $response->setBody(json_encode(['status' => 'Code 500: Es ist ein Fehler aufgetreten', 'msg' => $e->getMessage()]));
+        }
+        $response->send();
+    }
+}
